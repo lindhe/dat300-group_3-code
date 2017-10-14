c219824ea8ebd862530a3d10bccac5303c7e240b
Robert Gustafsson <robg@student.chalmers.se>
2017-10-14 10:29:44 +0200

    Add define to use policy drop first element

--- broccoli/src/midbropasad.c
+++ broccoli/src/midbropasad.c
@@ -6,7 +6,7 @@
 #include <broccoli.h>
 #endif
 
-Fifo_q * q;
+Fifo_q * queue;
 
     void
 request_n_values(int number, int arrayOfValues[])
@@ -14,7 +14,7 @@ request_n_values(int number, int arrayOfValues[])
     int i;
     Sensor_t * sensor;
     for(i=0; i<number; ++i){
-        sensor = pop_from_queue(q);
+        sensor = pop_from_queue(queue);
         arrayOfValues[i] = sensor->value;
         free(sensor);
     }
@@ -25,7 +25,7 @@ request_value()
 {
     int value;
     Sensor_t * sensor;
-    sensor = pop_from_queue(q);
+    sensor = pop_from_queue(queue);
     value = sensor->value;
     free(sensor);
     printf("Release 1 sensor data value\n");
@@ -35,11 +35,11 @@ request_value()
     void
 start_data_capture()
 {
-
-    q = init_queue(50);
     int res;
+    queue = init_queue(500000); /* Initiate queue with fixed size */
     pthread_t event_listener;
-    res = pthread_create(&event_listener, NULL, bro_event_listener, q);
+    /* Create producer thread that listen for bro events */
+    res = pthread_create(&event_listener, NULL, bro_event_listener, queue);
     if(res){
         perror("Unable to create thread");
         exit(-1);
