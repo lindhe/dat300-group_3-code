ca75b5fc5b505fa44a1f8705dd822dfcaa4ad455
Robin Krahl <me@robin-krahl.de>
2017-10-19 16:11:58 +0200

    Add script for live plotting of Pasad data

---
+++ broccoli/script/livegraph.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+	echo "Reads the sensor.dat and distance.dat from a running Pasad"
+	echo "instance and draws a graph from them."
+	echo
+	echo "Usage:"
+	echo "    $0 SOURCE"
+	echo "Arguments:"
+	echo "    SOURCE    an expression such that SOURCE/sensor.dat and"
+	echo "              SOURCE/distance.dat can be used as arguments for"
+	echo "              scp (e. g. user@host:/path/to/files)"
+	echo
+	echo "Note: Use ssh-add to avoid typing your SSH passphrase every second"
+	exit 1
+fi
+
+function plot() {
+	scp "${SCP_EXPR}/sensor.dat" "${SCP_EXPR}/distance.dat" .
+	echo "set terminal png; plot 'sensor.dat' using 0:1 with line, 'distance.dat' using 0:1 axis x1y2 with line"| gnuplot > live-tmp.png
+	mv live-tmp.png live.png
+}
+
+SCP_EXPR=$1
+
+echo 0 > sensor.dat
+echo 0 > distance.dat
+plot
+feh -x --reload 0.1 live.png &
+
+while true
+do
+	sleep 0.1
+	plot
+done
