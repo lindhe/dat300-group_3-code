fccd6e432065f3c1b59d795ab1c10d0aa2c64b3e
Robert Gustafsson <robg@student.chalmers.se>
2017-10-27 10:30:06 +0200

    Rename in midbro interface

--- broccoli/includes/midbro.h
+++ broccoli/includes/midbro.h
@@ -1,6 +1,6 @@
 #ifndef MIDBRO_H
 #define MIDBRO_H
-void request_n_values(int number, int arrayOfValues[]);
-int request_value();
 void start_data_capture();
+int request_value();
+void request_n_values(int n, int dest[]);
 #endif
