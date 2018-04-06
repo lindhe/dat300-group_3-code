679807de0c52b074bd3e0d62cfadf1c3a21dfc5c
Robert Gustafsson <robg@student.chalmers.se>
2018-04-06 11:38:25 +0200

    Add restart script

---
+++ script/restart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path=$1
+offset=$2
+
+if ! [ -s $path ]; then
+    exit 1
+fi
+
+now=$(date +%s)
+lastline=$(tail $path | grep "." | tail -1)
+lastts=$(echo "$lastline" | grep -oP '^\d+')
+
+if [ -z $lastts ]; then
+    exit 1
+fi
+
+tswithoffset=$(($lastts + $offset))
+if [ $now -gt $tswithoffset ]; then
+    echo "Restart"
+fi
